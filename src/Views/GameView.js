270b0dd8d7f8e81edd9d8ef755d4909253e19a0a
Charles BLANCHETON <charles.blancheton@gmail.com>
2020-03-23 18:55:49 +0100

    remove useless constructor

--- src/Views/GameView.js
+++ src/Views/GameView.js
@@ -2,10 +2,6 @@ import React from 'react';
 import FactoryView from './FactoryView';
 
 class GameView extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <FactoryView factory={this.props.game.factories[0]} />
