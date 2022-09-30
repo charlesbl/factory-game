9ff079437c33eef1d73abf1a06479e49ce1e32d9
Charles BLANCHETON <charles.blancheton@gmail.com>
2022-09-30 11:27:59 +0200

    add return button

--- src/Views/FactoryView.tsx
+++ src/Views/FactoryView.tsx
@@ -1,19 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import '../css/Factory.css'
 import Factory from '../Game/Factory'
 import Machine from '../Game/Machine'
 import MachineView from './MachineView'
 import FactoryCardView from './FactoryCardView'
 import IBaseProps from './IBaseProps'
-import SelectMachineView from './SelectMachineView'
-import MachineCraft from '../Game/MachineCraft'
-import InventoryView from './InventoryView'
-import { notImplemented } from '..'
 
 interface IFactoryProps extends IBaseProps {
     factory: Factory
     onSelectedFactory: (factory: Factory) => void
-    onGoBack: () => void
 }
 
 const renderMachine = (machine: Machine, index: number): JSX.Element => {
@@ -24,39 +19,24 @@ const renderMachine = (machine: Machine, index: number): JSX.Element => {
     )
 }
 
-const renderFactoryCard = (factory: Factory, index: number, onSelectedFactory: (factory: Factory) => void): JSX.Element => {
+const renderFactoryCard = (factory: Factory, index: number, onClickEnter: () => void): JSX.Element => {
     return (
         <div key={index} className="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 machine-container">
-            <FactoryCardView factory={factory} onClickEnter={() => onSelectedFactory(factory)} />
+            <FactoryCardView factory={factory} onClickEnter={onClickEnter} />
         </div>
     )
 }
 
 const FactoryView = (props: IFactoryProps): JSX.Element => {
-    const [selectedMachineCraft, selectMachineCraft] = useState<MachineCraft | undefined>(undefined)
-
     const machines = props.factory.machines.map((machine, i) => renderMachine(machine, i))
-    const factories = props.factory.factories.map((factory, i) => renderFactoryCard(factory, i, props.onSelectedFactory))
-    const returnButton = <button className="btn btn-primary" onClick={() => props.onGoBack()}>Return</button>
+    const factories = props.factory.factories.map((factory, i) => renderFactoryCard(factory, i, () => props.onSelectedFactory(factory)))
     return (
         <div>
-            <InventoryView game={props.game} inventory={props.game.inventory} />
-            {returnButton}
             <div className="row">
                 {machines}
             </div>
             <div className="row">
                 {factories}
-            </div>
-            <div className="col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2 machine-container">
-                <button className="btn btn-primary"
-                    onClick={() => props.factory.addSubFactory()}>Add factory</button>
-                <SelectMachineView onChange={(machineCraft) => selectMachineCraft(machineCraft)} />
-                <button className="btn btn-primary"
-                    onClick={() => {
-                        console.log(selectedMachineCraft)
-                        notImplemented()
-                    }}>Add machine</button>
             </div>
         </div>
     )
--- src/Views/GameView.tsx
+++ src/Views/GameView.tsx
@@ -2,25 +2,46 @@ import React, { useState } from 'react'
 import FactoryView from './FactoryView'
 import IBaseProps from './IBaseProps'
 import Factory from '../Game/Factory'
-import { notImplemented } from '..'
-
-const goBack = (): void => {
-    notImplemented()
-    // if (this.state.selectedFactory.topFactory)
-    //     this.changeFactory(this.state.selectedFactory.topFactory);
-}
+import InventoryView from './InventoryView'
+import SelectMachineView from './SelectMachineView'
+import MachineCraft from '../Game/MachineCraft'
 
 const GameView = (props: IBaseProps): JSX.Element => {
-    const [selectedFactory, setFactory] = useState<Factory>(props.game.factory)
+    const [selectedMachineCraft, selectMachineCraft] = useState<MachineCraft | undefined>(undefined)
+    const [factories, setFactories] = useState<Factory[]>([props.game.factory])
+    const currentFactory = factories[factories.length - 1]
+
+    const returnButton = factories.length === 1
+        ? ''
+        : <button className="btn btn-primary" onClick={() => {
+            factories.pop()
+            setFactories([...factories])
+        }}>Return</button>
 
     return (
         <div>
             <span>{props.game.money.toFixed(2)}€</span>
+            <InventoryView game={props.game} inventory={props.game.inventory} />
+            {returnButton}
             <FactoryView
                 game={props.game}
-                factory={selectedFactory}
-                onSelectedFactory={(factory) => setFactory(factory)}
-                onGoBack={() => goBack()} />
+                factory={currentFactory}
+                onSelectedFactory={(factory) => {
+                    factories.push(factory)
+                    setFactories([...factories])
+                }} />
+
+            <div className="col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2">
+                <button className="btn btn-primary"
+                    onClick={() => currentFactory.addSubFactory()}>Add factory</button>
+                <SelectMachineView onChange={(machineCraft) => selectMachineCraft(machineCraft)} />
+                <button className="btn btn-primary"
+                    onClick={() => {
+                        if (selectedMachineCraft === undefined) return
+                        console.log(selectedMachineCraft)
+                        currentFactory.buildMachine(selectedMachineCraft)
+                    }}>Add machine</button>
+            </div>
         </div>
     )
 }
